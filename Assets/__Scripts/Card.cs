65d35eb82064a7d766a43152b5718febdb8d0da0
Jiansong He <jiansonghe@live.cn>
2021-03-29 16:16:37 -0400

    adding Card and Deck

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -16,3 +16,18 @@ public class Card : MonoBehaviour
         
     }
 }
+
+[System.Serializable]
+public class Decorator {
+    public string type; 
+    public Vector3 loc; //location of sprite on the card
+    public bool flip = false; 
+    public float scale = 1f;
+}
+
+[System.Serializable]
+public class CardDefinition {
+    public string face; 
+    public int rank;
+    public List<Decorator> pip = new List<Decorator>();
+}
